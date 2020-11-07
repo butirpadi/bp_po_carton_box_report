26e397601952918cc5c5f5bf3971586631f617f0
butirpadi <butirpadi@gmail.com>
2020-11-07 07:44:16 +0700

    Fixing report carton box

--- models/product_template.py
+++ models/product_template.py
@@ -17,10 +17,6 @@ class JenisCarton(models.Model):
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    # is_carton_box = fields.Boolean(string='Carton/Box')
     warna_box_id = fields.Many2one('warna.box', string='Warna')
     jenis_carton_id = fields.Many2one('jenis.carton', string='Jenis Carton')
-    # box_length = fields.Integer(string='Length', default=None)
-    # box_width = fields.Integer(string='Width', default=None)
-    # box_height = fields.Integer(string='Height', default=None)
     isi_per_box = fields.Integer(string='Isi per Box', default=None)
