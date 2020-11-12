f3470740bd0562e5e70a52534282d7147c7461e6
butirpadi <butirpadi@gmail.com>
2020-11-12 15:27:57 +0700

    Add auto calculate volume on product template.

--- models/product_template.py
+++ models/product_template.py
@@ -21,3 +21,8 @@ class ProductTemplate(models.Model):
     jenis_carton_id = fields.Many2one('jenis.carton', string='Jenis Carton')
     isi_per_box = fields.Integer(string='Isi per Box', default=None)
     is_carton_box = fields.Boolean(string='Karton Box', default=False)
+
+    @api.onchange('length', 'width', 'height')
+    def _auto_calculate_volume(self):
+        if self.length and self.width and self.height:
+            self.volume = float(self.length) * float(self.width) * float(self.height)
