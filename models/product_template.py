9c2d8ef55700d8172bd0064e2d3f9c2b3909d3d1
butirpadi <butirpadi@gmail.com>
2020-12-29 11:41:19 +0700

    Ganti Perhitungan Volume dan tambah precision field volume jadi 4 digit

--- models/product_template.py
+++ models/product_template.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+from pprint import pprint
 
 
 class WarnaBox(models.Model):
@@ -22,6 +23,9 @@ class ProductTemplate(models.Model):
     isi_per_box = fields.Integer(string='Isi per Box', default=None)
     is_carton_box = fields.Boolean(string='Karton Box', default=False)
 
+    volume = fields.Float('Volume', compute='_compute_volume', inverse='_set_volume',
+                          help="The volume in m3.", store=True, digits=(16, 4))
+
     # replace from module wk_product_dimensions
     length = fields.Float(string='Length',)
     width = fields.Float(string='Width',)
@@ -32,19 +36,37 @@ class ProductTemplate(models.Model):
     temp_width = fields.Float(string='Width',)
     temp_height = fields.Float(string='Height',)
 
-    @api.onchange('length', 'width', 'height')
-    def _auto_calculate_volume(self):
-        factor = self.dimensions_uom_id.factor
-        if self.dimensions_uom_id.uom_type == 'smaller':
-            factor = 1/factor
-        l = factor * self.length
-        w = factor * self.width
-        h = factor * self.height
-        self.update({
-            'volume': l * w * h
-        })
+    # @api.onchange('length', 'width', 'height')
+    # def _auto_calculate_volume(self):
+    #     factor = self.dimensions_uom_id.factor
+    #     if self.dimensions_uom_id.uom_type == 'smaller':
+    #         factor = 1/factor
+    #     l = factor * self.length
+    #     w = factor * self.width
+    #     h = factor * self.height
+    #     self.update({
+    #         'volume': l * w * h
+    #     })
 
-        # if self.length and self.width and self.height:
-        #     self.volume = self.length * self.width * self.height
-        #     # self.volume = float(self.length) * \
-        #     #     float(self.width) * float(self.height)
+    @api.multi
+    def write(self, values):
+        pprint(values)
+        if 'length' in values or 'width' in values or 'height' in values:
+            factor = self.dimensions_uom_id.factor
+            print('factor : ' + str(factor))
+            if self.dimensions_uom_id.uom_type == 'smaller':
+                factor = 1/factor
+            print('factor 2 : ' + str(factor))
+            l = factor * (values['length']
+                          if 'length' in values else self.length)
+            w = factor * (values['width'] if 'width' in values else self.width)
+            h = factor * (values['height']
+                          if 'height' in values else self.height)
+            print('L : ' + str(l))
+            print('W : ' + str(w))
+            print('H : ' + str(h))
+            values['volume'] = l*w*h
+            print('Vol : ' + str(l*w*h))
+
+        # CODE HERE
+        return super(ProductTemplate, self).write(values)
